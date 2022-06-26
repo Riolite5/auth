25217856cd9921859c792a18bb367452f422ed3c
paradox <tariq.khasawneh.93@gmail.com>
2022-06-26 14:37:41 +0300

    first commit

---
+++ config/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5JUqRpYgPtyBDOYBCPju4SZsZxt0V7O4",
+  authDomain: "auth-3079a.firebaseapp.com",
+  projectId: "auth-3079a",
+  storageBucket: "auth-3079a.appspot.com",
+  messagingSenderId: "10329387189",
+  appId: "1:10329387189:web:72ee3224934f0c498dcefc",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
